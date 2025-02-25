9c79d5cee403ef39d24fc2ac5fde02fccd899729
tapas-code <tapas.code247@gmail.com>
2025-02-25 09:04:45 +0530

    server: Connect Mongodb

---
+++ server/src/server.ts
@@ -0,0 +1,29 @@
+import express, { Application, Request, Response } from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Application = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Basic route
+app.get("/", (req: Request, res: Response) => {
+  res.send("Task Management API is running");
+});
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGO_URI as string)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("MongoDB connection error:", error);
+  });
