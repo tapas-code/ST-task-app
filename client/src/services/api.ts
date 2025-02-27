d01a8fe514399e55dd877a6a14a677106dba4127
tapas-code <tapas.code247@gmail.com>
2025-02-27 13:16:25 +0530

    client: Update backend url

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:8080/api"
+// const API_BASE_URL = "http://localhost:8080/api"
+const API_BASE_URL = "https://st-task-app-api.onrender.com/api"
 
 const api = axios.create({
     baseURL: API_BASE_URL,
